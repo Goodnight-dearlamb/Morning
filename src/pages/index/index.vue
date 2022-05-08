a38669222f347bd0276de80a4bd28e1c1cfb47cf
wanglihao <2524520356@qq.com>
2022-05-08 17:09:16 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
